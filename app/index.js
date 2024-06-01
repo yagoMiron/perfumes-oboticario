4bee68d3203b9b3b58621c47c497ecf4bb8c1142
Yago Verzas Miron <105978576+yagoMiron@users.noreply.github.com>
2024-06-01 16:51:31 -0400

    adicionando javascript

---
+++ app/index.js
@@ -0,0 +1,78 @@
+const radios = document.querySelectorAll(".radio_btn");
+const delay = (millisegundos) => {
+  return new Promise((resolve) => setTimeout(resolve, millisegundos));
+};
+
+var SVG = document.getElementById("logo");
+SVG.addEventListener(
+  "load",
+  function () {
+    setThema(achaPosicao());
+  },
+  false
+);
+
+for (let index = 0; index < radios.length; index++) {
+  radios[index].addEventListener("click", () => {
+    setThema(index);
+  });
+}
+
+for (let index = 0; index < 15; index++) {
+  await delay(10000);
+  passaSlide();
+}
+
+function passaSlide() {
+  const posicaoAtual = achaPosicao();
+  const proximaPosicao = posicaoAtual + 1 >= 4 ? 0 : posicaoAtual + 1;
+  radios[proximaPosicao].checked = true;
+  setThema(proximaPosicao);
+}
+
+function achaPosicao() {
+  for (let index = 0; index < radios.length; index++) {
+    if (radios[index].checked == true) {
+      return index;
+    }
+  }
+  return 0;
+}
+function setThema(temaId) {
+  switch (temaId) {
+    case 0:
+      document.documentElement.style.setProperty("--header-main", "#ffbc6fb5");
+      document.documentElement.style.setProperty("--footer-border", "#d38519");
+      document.documentElement.style.setProperty("--svg-menu", "url(../icons/menu-slide1.svg)");
+      document.getElementById("logo").contentDocument.all[1].attributes[
+        "fill"
+      ].value = "#bd7615";
+      break;
+    case 1:
+      document.documentElement.style.setProperty("--header-main", "#ffd3e4");
+      document.documentElement.style.setProperty("--footer-border", "#b50036");
+      document.documentElement.style.setProperty("--svg-menu", "url(../icons/menu-slide2.svg)");
+      document.getElementById("logo").contentDocument.all[1].attributes[
+        "fill"
+      ].value = "#b50036";
+      break;
+    case 2:
+      document.documentElement.style.setProperty("--header-main", "#ffe771");
+      document.documentElement.style.setProperty("--footer-border", "#b08802");
+      document.documentElement.style.setProperty("--svg-menu", "url(../icons/menu-slide3.svg)");
+      document.getElementById("logo").contentDocument.all[1].attributes[
+        "fill"
+      ].value = "#b08802";
+      break;
+    case 3:
+      document.documentElement.style.setProperty("--header-main", "#01494b");
+      document.documentElement.style.setProperty("--footer-border", "#daa520");
+      document.documentElement.style.setProperty("--svg-menu", "url(../icons/menu-slide4.svg)");
+      document.getElementById("logo").contentDocument.all[1].attributes[
+        "fill"
+      ].value = "#daa520";
+      break;
+    default:
+      break;
+  }
+}
